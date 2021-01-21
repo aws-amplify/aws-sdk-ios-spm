5d985fcab1dc1d69ee5bbc14b6081ca7e9e9c8b1
Jithin Roy <royji@amazon.com>
2021-01-20 18:13:17 -0800

    feat: Support for Swift Package Manager added (#1)

---
+++ Package.swift
@@ -0,0 +1,80 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+// Current stable version of the AWS iOS SDK
+// 
+// This value will be updated by the CI/CD pipeline and should not be
+// updated manually
+let latestVersion = "2.22.1"
+
+// Domain where the release artifacts are hosted.
+let hostingDomain = "<TBD>"
+
+// Map between the available frameworks and the checksum
+//
+// The checksum value will be updated by the CI/CD pipeline and should 
+// not be updated manually
+let frameworksToChecksum = [
+    "AWSAPIGateway": "SHA256_CHECKSUM",
+    "AWSAppleSignIn": "SHA256_CHECKSUM",
+    "AWSAuthCore": "SHA256_CHECKSUM",
+    "AWSAuthUI": "SHA256_CHECKSUM",
+    "AWSAutoScaling": "SHA256_CHECKSUM",
+    "AWSCloudWatch": "SHA256_CHECKSUM",
+    "AWSCognitoAuth": "SHA256_CHECKSUM",
+    "AWSCognitoIdentityProvider": "SHA256_CHECKSUM",
+    "AWSCognitoIdentityProviderASF": "SHA256_CHECKSUM",
+    "AWSComprehend": "SHA256_CHECKSUM",
+    "AWSConnect": "SHA256_CHECKSUM",
+    "AWSConnectParticipant": "SHA256_CHECKSUM",
+    "AWSCore": "SHA256_CHECKSUM",
+    "AWSDynamoDB": "SHA256_CHECKSUM",
+    "AWSEC2": "SHA256_CHECKSUM",
+    "AWSElasticLoadBalancing": "SHA256_CHECKSUM",
+    "AWSFacebookSignIn": "SHA256_CHECKSUM",
+    "AWSGoogleSignIn": "SHA256_CHECKSUM",
+    "AWSIoT": "SHA256_CHECKSUM",
+    "AWSKMS": "SHA256_CHECKSUM",
+    "AWSKinesis": "SHA256_CHECKSUM",
+    "AWSKinesisVideo": "SHA256_CHECKSUM",
+    "AWSKinesisVideoArchivedMedia": "SHA256_CHECKSUM",
+    "AWSKinesisVideoSignaling": "SHA256_CHECKSUM",
+    "AWSLambda": "SHA256_CHECKSUM",
+    "AWSLex": "SHA256_CHECKSUM",
+    "AWSLocation": "SHA256_CHECKSUM",
+    "AWSLogs": "SHA256_CHECKSUM",
+    "AWSMachineLearning": "SHA256_CHECKSUM",
+    "AWSMobileClientXCF": "SHA256_CHECKSUM",
+    "AWSPinpoint": "SHA256_CHECKSUM",
+    "AWSPolly": "SHA256_CHECKSUM",
+    "AWSRekognition": "SHA256_CHECKSUM",
+    "AWSS3": "SHA256_CHECKSUM",
+    "AWSSES": "SHA256_CHECKSUM",
+    "AWSSNS": "SHA256_CHECKSUM",
+    "AWSSQS": "SHA256_CHECKSUM",
+    "AWSSageMakerRuntime": "SHA256_CHECKSUM",
+    "AWSSimpleDB": "SHA256_CHECKSUM",
+    "AWSTextract": "SHA256_CHECKSUM",
+    "AWSTranscribe": "SHA256_CHECKSUM",
+    "AWSTranscribeStreaming": "SHA256_CHECKSUM",
+    "AWSTranslate": "SHA256_CHECKSUM",
+    "AWSUserPoolsSignIn": "SHA256_CHECKSUM"
+]
+
+var products = frameworksToChecksum.keys.map {Product.library(name: $0, targets: [$0])}
+
+var targets = frameworksToChecksum.map { framework, checksum in
+    Target.binaryTarget(name: framework,
+                        url: "\(hostingDomain)/\(framework)-\(latestVersion).zip",
+                        checksum: checksum)
+}
+
+let package = Package(
+    name: "AWSiOSSDKV2",
+    platforms: [
+        .iOS(.v9)
+    ],
+    products: products,
+    targets: targets
+)
+
--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# aws-sdk-ios-spm
+# Swift Package Manager support for AWS SDK for iOS
+
+This repository enables Swift Package Manager support for the AWS Mobile SDK for iOS by vending a Manifest file (`Package.swift`) that links to binary targets for the SDKs.
+
+## Setup
+
+To get started with the AWS SDK for iOS, check out the [Developer Guide for iOS](https://aws-amplify.github.io/docs/ios/start). You can set up the SDK and start building a new project, or you integrate the SDK in an existing project.
+
+To use the AWS SDK for iOS, you will need the following installed on your development machine:
+
+* Xcode 11.0 or later
+* iOS 9 or later
+
+
+## Adding AWS SDK iOS via Swift Package Manager
+
+1. Open your project in Xcode 11.0 or above
+
+2. Go to **File** > **Swift Packages** > **Add Package Dependency...**
+
+3. In the field **Enter package repository URL**, enter "https://github.com/aws-amplify/aws-sdk-ios-spm"
+
+4. Pick the latest version and click **Next**.
+
+    **NOTE:** The AWS Mobile SDK for iOS [does not follow Semantic Versioning](https://docs.amplify.aws/sdk/configuration/setup-options/q/platform/ios#aws-sdk-version-vs-semantic-versioning).
+
+5. Choose the packages required for your project and click **Finish**
