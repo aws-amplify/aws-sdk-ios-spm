bda431b9e3ab817f0eb59f97b7b95c8a52503cfc
Brennan Stehling <stehlib@amazon.com>
2021-11-04 09:36:58 -0700

    feat(CI/CD): supports resuming local builds for XCF

--- local.sh
+++ local.sh
@@ -6,24 +6,23 @@
 export SOURCE_REPO_DIR=../aws-sdk-ios
 export XCF_OUTPUT_DIR=xcframeworks/output/XCF
 
+# clear the output
+# rm -rf "${SOURCE_REPO_DIR}/${XCF_OUTPUT_DIR}"
+
 if [ ! -d "${SOURCE_REPO_DIR}" ]; then
     echo "AWS SDK iOS repo is required: ${SOURCE_REPO_DIR}" 
     exit 0
 fi
 
-if [ ! -d "${SOURCE_REPO_DIR}/${XCF_OUTPUT_DIR}" ]; then
-    # builds packages and produces XCF files
-    pushd .
-    cd "${SOURCE_REPO_DIR}"
-    python3 ./CircleciScripts/create_xcframeworks.py
-    popd
-fi
+# builds packages and produces XCF files
+pushd .
+cd "${SOURCE_REPO_DIR}"
+python3 ./CircleciScripts/create_xcframeworks.py
+popd
 
-if [ ! -d "XCF" ]; then
-    # Copy XCF output into SPM repo
-    mkdir -p XCF
-    cp -rp "${SOURCE_REPO_DIR}/${XCF_OUTPUT_DIR}" .
-fi
+# Copy XCF output into SPM repo
+mkdir -p XCF
+cp -rp "${SOURCE_REPO_DIR}/${XCF_OUTPUT_DIR}" .
 
 # Manually enable local repo when it is being used locally
 echo "Update ${XCF_OUTPUT_DIR}/Package.swift to use local XCF files"
